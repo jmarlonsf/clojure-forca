241a7a68bce9d278fa0604c555447c7307d142e8
João Marlon <marlonsouto@gmail.com>
2019-07-06 22:32:17 -0300

    lendo do teclado e relacionamento entre funções

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,6 +1,8 @@
 (ns forca.core
   (:gen-class))
 
+(declare jogo)
+
 (def total-de-vidas 6)
 
 (defn perdeu [] (print "Você perdeu!"))
@@ -13,13 +15,23 @@
 (defn acertou-a-palavra-toda? [palavra acertos] 
     (empty? (letras-faltantes palavra acertos))
 )
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+    (if (acertou? chute palavra)
+        (jogo vidas palavra (conj acertos chute))
+        (jogo (dec vidas) palavra acertos)
+    )  
+)
 
 (defn jogo [vidas palavra acertos]
   (if (= vidas 0)
       (perdeu)
       (if (acertou-a-palavra-toda? palavra acertos)
           (ganhou)
-          (print "Chuta, amigo!")   
+          (avalia-chute (le-letra!) vidas palavra acertos)   
       )
       
   )
