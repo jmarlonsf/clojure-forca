6ee66155ce654b79e23c6484b390ad0b86ad9420
João Marlon <marlonsouto@gmail.com>
2019-07-06 23:25:21 -0300

    funções recursivas e recursão em cauda

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,8 +1,6 @@
 (ns forca.core
   (:gen-class))
 
-(declare jogo)
-
 (def total-de-vidas 6)
 
 (defn perdeu [] (print "Você perdeu!"))
@@ -19,23 +17,20 @@
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn avalia-chute [chute vidas palavra acertos]
-    (if (acertou? chute palavra)
-        (jogo vidas palavra (conj acertos chute))
-        (jogo (dec vidas) palavra acertos)
-    )  
-)
-
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-      (perdeu)
-      (if (acertou-a-palavra-toda? palavra acertos)
-          (ganhou)
-          (avalia-chute (le-letra!) vidas palavra acertos)   
-      )
-      
-  )
-)
+  (cond (= vidas 0)(perdeu)
+      (acertou-a-palavra-toda? palavra acertos)(ganhou)
+      :else
+      (let [chute (le-letra!)]
+          (if (acertou? chute palavra)
+            (do
+              (println "Acertou a letra!")
+              (recur vidas palavra (conj acertos chute))
+            )
+            (do
+              (println "Errou a letra!")
+              (recur (dec vidas) palavra acertos))))))
+            
 
 (defn -main
   "I don't do a whole lot ... yet."
